d5e2a34045eb7deb24584b8f723debb1080fbbf1
Youngheon Choi <102468071+eEonTown@users.noreply.github.com>
2022-07-25 23:54:55 +0900

    Add : FAQ작성 페이지 화면 구현
    add : faqInsertPage.jsp
    - 관리자 FAQ 작성 페이지 화면 구현
    
    add : FAQInsertPage.java
    - 페이지 이동용도 포워딩하는 서블릿 추가
    
    update : noticeManagement.jsp
    - 서블릿 호출

---
+++ src/main/java/com/jj/admin/controller/FAQInsertPage.java
@@ -0,0 +1,42 @@
+package com.jj.admin.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NoticeInsertPage
+ */
+@WebServlet("/faqInsert.ad")
+public class FAQInsertPage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FAQInsertPage() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 페이지 이동용 포워딩만 작성 했음
+		// 화면 구현 완료 후 수정 예정
+		request.getRequestDispatcher("views/admin/faqInsertPage.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
