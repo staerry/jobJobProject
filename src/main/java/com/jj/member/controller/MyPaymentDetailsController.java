6d08843bb7421fc570c1f4a648cc423474f89981
cruella13 <100200569+cruella13@users.noreply.github.com>
2022-07-27 17:25:28 +0900

    Revert "etc"
    This reverts commit 6f367a690098b1b825d4480c72f5b6c26be425bb.

--- src/main/java/com/jj/member/controller/MyPaymentDetailsController.java
+++ src/main/java/com/jj/member/controller/MyPaymentDetailsController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MyPaymentDetailsController
  */
-@WebServlet("/MyPaymentDetailsController")
+@WebServlet("/paymentDetails.me")
 public class MyPaymentDetailsController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
