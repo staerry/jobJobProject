e68e2eb999050bf15317cae0ff23087fe8bd3d9e
youngheonchoi <gjswns21@naver.com>
2022-08-02 16:04:00 +0900

    Etc : 코드 정리

--- src/main/java/com/jj/admin/controller/FAQInsertPage.java
+++ src/main/java/com/jj/admin/controller/FAQInsertPage.java
@@ -26,7 +26,7 @@ public class FAQInsertPage extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		request.getRequestDispatcher("views/admin/faqInsertPage.jsp").forward(request, response);
 	}
 
 	/**
